50b7b73ace9ad817256df5fcc5281e167e75ef5c
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-01-07 11:03:02 -0500

    Etapa 5

---
+++ Entidades/Alumno.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace CoreEscuela.Entidades
+{
+    public class Alumno : ObjetoEscuelaBase
+    {
+        public List<Evaluación> Evaluaciones { get; set; } = new List<Evaluación>();
+    }
+}
